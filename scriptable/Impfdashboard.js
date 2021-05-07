cc22787cae6685bd7121f7e7eaa554e9cbf07eb4
Christian Lobach <christian.lobach@gmail.com>
2021-05-07 15:11:47 +0200

    lint widget

--- scriptable/Impfdashboard.js
+++ scriptable/Impfdashboard.js
@@ -25,52 +25,52 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-let widget = new ListWidget();
-const locale = Device.locale
+const widget = new ListWidget();
+const { locale } = Device;
 const horizontalPadding = 12;
 const verticalPadding = 8;
 widget.setPadding(verticalPadding, 0, verticalPadding, 0);
-widget.url = "https://impfdashboard.de";
+widget.url = 'https://impfdashboard.de';
 
 let data = await loadData();
 let chart = await loadChart();
 
-let headerStack = widget.addStack();
+const headerStack = widget.addStack();
 headerStack.setPadding(0, horizontalPadding, 0, horizontalPadding);
-let header = headerStack.addText("💉 Impfdashboard".toUpperCase());
+const header = headerStack.addText('💉 Impfdashboard'.toUpperCase());
 header.font = Font.mediumSystemFont(10);
 
 widget.addSpacer(4);
 
-let hStack = widget.addStack();
+const hStack = widget.addStack();
 hStack.setPadding(0, horizontalPadding, 0, horizontalPadding);
 hStack.layoutHorizontally();
 
-let firstStack = hStack.addStack();
+const firstStack = hStack.addStack();
 firstStack.layoutVertically();
-let firstTitle = firstStack.addText(data.firstVaccinations.title);
+const firstTitle = firstStack.addText(data.firstVaccinations.title);
 firstTitle.font = Font.mediumSystemFont(10);
-let firstValue = firstStack.addText(data.firstVaccinations.stringValue);
+const firstValue = firstStack.addText(data.firstVaccinations.stringValue);
 firstValue.font = Font.mediumSystemFont(16);
-firstValue.textColor = new Color("#3392C5");
+firstValue.textColor = new Color('#3392C5');
 
 hStack.addSpacer();
 
-let fullStack = hStack.addStack();
+const fullStack = hStack.addStack();
 fullStack.layoutVertically();
-let fullTitle = fullStack.addText(data.fullVaccinations.title);
+const fullTitle = fullStack.addText(data.fullVaccinations.title);
 fullTitle.font = Font.mediumSystemFont(10);
-let fullValue = fullStack.addText(data.fullVaccinations.stringValue);
+const fullValue = fullStack.addText(data.fullVaccinations.stringValue);
 fullValue.font = Font.mediumSystemFont(16);
-fullValue.textColor = new Color("#3392C5");
+fullValue.textColor = new Color('#3392C5');
 
 widget.addImage(chart).centerAlignImage();
 
-let todayValue = widget.addText(data.dosesToday.stringValue);
+const todayValue = widget.addText(data.dosesToday.stringValue);
 todayValue.centerAlignText();
 todayValue.font = Font.mediumSystemFont(16);
-todayValue.textColor = new Color("#3392C5");
-let todayTitle = widget.addText(data.dosesToday.title);
+todayValue.textColor = new Color('#3392C5');
+const todayTitle = widget.addText(data.dosesToday.title);
 todayTitle.centerAlignText();
 todayTitle.font = Font.mediumSystemFont(10);
 
@@ -79,38 +79,37 @@ Script.complete();
 widget.presentSmall();
 
 async function loadData() {
-  let url =
-    "https://github.com/DerLobi/impfdashboard-scriptable-widget/raw/main/data/data.json";
-  let request = new Request(url);
-  let records = await request.loadJSON();
+  const url = 'https://github.com/DerLobi/impfdashboard-scriptable-widget/raw/main/data/data.json';
+  const request = new Request(url);
+  const records = await request.loadJSON();
 
-  let lastRecord = records[records.length - 1];
+  const lastRecord = records[records.length - 1];
 
-  let data = {
+  const data = {
     firstVaccinations: {
-      title: "Mindestens\nErstgeimpfte",
+      title: 'Mindestens\nErstgeimpfte',
       stringValue: lastRecord.impf_quote_erst.toLocaleString(locale, {
-        style: "percent",
+        style: 'percent',
         minimumFractionDigits: 0,
         maximumFractionDigits: 2,
       }),
     },
     fullVaccinations: {
-      title: "Vollständig\nGeimpfte",
+      title: 'Vollständig\nGeimpfte',
       stringValue: lastRecord.impf_quote_voll.toLocaleString(locale, {
-        style: "percent",
+        style: 'percent',
         minimumFractionDigits: 0,
         maximumFractionDigits: 2,
       }),
     },
     dosesToday: {
       title: new Date(lastRecord.date).toLocaleString(locale, {
-        weekday: "long",
-        year: "2-digit",
-        month: "numeric",
-        day: "numeric",
+        weekday: 'long',
+        year: '2-digit',
+        month: 'numeric',
+        day: 'numeric',
       }),
-      stringValue: "+" + lastRecord.dosen_differenz_zum_vortag.toLocaleString(),
+      stringValue: `+${lastRecord.dosen_differenz_zum_vortag.toLocaleString()}`,
     },
   };
 
@@ -118,9 +117,8 @@ async function loadData() {
 }
 
 async function loadChart() {
-  let url =
-    "https://github.com/DerLobi/impfdashboard-scriptable-widget/raw/main/data/barChart.png";
-  let request = new Request(url);
-  let image = await request.loadImage();
+  const url = 'https://github.com/DerLobi/impfdashboard-scriptable-widget/raw/main/data/barChart.png';
+  const request = new Request(url);
+  const image = await request.loadImage();
   return image;
 }

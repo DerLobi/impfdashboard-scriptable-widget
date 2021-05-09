0effeeef02c485f17ce52887a88c42f6e2221853
Christian Lobach <christian.lobach@gmail.com>
2021-05-09 14:45:01 +0200

    delete server side chart creation code

--- src/index.js
+++ src/index.js
@@ -1,29 +1,8 @@
 const core = require('@actions/core');
 const downloadData = require('./downloadData.js');
-const createChart = require('./createChart.js');
 
 downloadData((twoWeeksData, downloadError) => {
-  const weeklyData = twoWeeksData.slice(twoWeeksData.length - 7, twoWeeksData.length);
   if (downloadError) {
     core.setFailed(downloadError.message);
-    return;
   }
-
-  createChart(weeklyData, 'small', './data/barChart.png', (error) => {
-    if (error) {
-      core.setFailed(error.message);
-    }
-  });
-
-  createChart(weeklyData, 'medium', './data/barChart-medium.png', (error) => {
-    if (error) {
-      core.setFailed(error.message);
-    }
-  });
-
-  createChart(twoWeeksData, 'large', './data/barChart-large.png', (error) => {
-    if (error) {
-      core.setFailed(error.message);
-    }
-  });
 });
